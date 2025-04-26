a1ecd586aea1917415c7340e2569a1bbf95f8e31
Ahmed-Elgammal-900 <genesis570809@gmail.com>
2025-04-26 22:13:38 +0300

    handle for production

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -27,7 +27,7 @@ export class AuthController {
     res.cookie('token', token, {
       httpOnly: true,
       maxAge: 3600000,
-      sameSite: 'lax',
+      sameSite: 'none',
       secure: process.env.ENVIROMENT === 'production',
       priority: 'high',
     });
